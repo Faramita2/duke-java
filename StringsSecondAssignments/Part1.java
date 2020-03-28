be8788e2bc398838189a276e6cb3dfa30ad83224
meow <liuguozhu720@gmail.com>
2020-03-28 12:43:46 +0800

    Create StringSecondAssignments project

---
+++ StringsSecondAssignments/Part1.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of Part1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Part1 {
+    
+    
+    public int findStopCodon(String dna, int startIndex, String stopCodeon) {
+        
+        
+        return dna.length();
+    }
+    
+    public void testFindStopCodon() {
+        String stopCodeon = "TAA";
+        
+        
+        stopCodeon = "TAG";
+        
+        
+        stopCodeon = "TGA";
+    }
+}
